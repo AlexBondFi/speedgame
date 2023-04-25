37d36cfe4551edddcab9bf22101ed56c460a05b5
Alex Bond <s2300120@edu.bc.fi>
2023-04-25 14:58:49 +0300

    overlay cls btn, block buttons, speed, lvls left

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import Circle from './Circle';
 import Modal from './Modal';
+import startSound from './start.mp3';
+import endSound from './end.mp3';
+import clickSound from './click.mp3'
 
 class App extends Component {
     state = {
       score: 0,
       current: 0,
+      rounds: 0,
       showModal: false,
       circles: [1,2,3],
       gameStart: false,
-      showGameOver: false
+      showGameOver: false,
+      soundStart: new Audio(startSound),
+      soundEnd: new Audio(endSound),
+      soundClick: new Audio(clickSound)
     }
 
     modalHandler = () => {
@@ -20,41 +27,48 @@ class App extends Component {
     }
 
     clickHandler = (circle) => {
+      this.state.soundClick.play();
       console.log(circle)
       if(circle !== this.state.current){
         this.endHandler();
       }
       this.setState({
         score: this.state.score +10,
+        rounds: 0
       });
-      this.randomizer();
     }
 
     randomizer = () => {
       console.log('rand')
       let nextActive;
-      do {
-        nextActive = Math.floor(Math.random() * 3) + 1;
-      } while (nextActive === this.state.current);
+      if(this.state.rounds >=3){
+        return this.endHandler()
+      }
+      else{
+        do {
+          nextActive = Math.floor(Math.random() * 3) + 1;
+        } while (nextActive === this.state.current);
+      }
+      
   
       this.setState({
-        current: nextActive
+        current: nextActive,
+        rounds: this.state.rounds +1
       });
     };
 
-    
-
-
-
     startHandler = () => {
+      this.state.soundStart.play();
       this.timer = setInterval(()=>{this.randomizer()}, 1000);
       this.setState({
         gameStart: true
+        
       })
     };
     
     
     endHandler = () => {
+      this.state.soundEnd.play();
       clearInterval(this.timer);
       this.modalHandler();
       this.setState({
